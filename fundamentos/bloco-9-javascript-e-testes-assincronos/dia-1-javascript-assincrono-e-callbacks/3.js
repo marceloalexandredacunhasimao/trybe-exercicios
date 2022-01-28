d43b1f5e1f1d762e8097ebc578b7703c32136b36
marceloalexandredacunhasimao <marceloalexandredacunhasimao@gmail.com>
2022-01-28 03:17:45 -0300

    Exercícios 9.1 (28/01/2022) concluídos

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/3.js
@@ -0,0 +1,31 @@
+const getPlanet = () => {
+setTimeout(() => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+  },4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
+
+
+/*
+//GABARITO:
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers"
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
+*/
---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/8.js
@@ -0,0 +1,48 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+function func(error,message) {
+	if(error === null) {
+		console.log(message);
+	}
+	else {
+		console.log(error);
+	}
+}
+
+getPokemonDetails((pokemon) => pokemon.name === 'Bulbasaur',func);
+getPokemonDetails((pokemon) => pokemon.name === 'NaoExiste',func);
+
+module.exports = {
+  getPokemonDetails,
+};
